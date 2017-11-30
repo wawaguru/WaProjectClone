d8e6df7e16f6ffccc730b496e27f0d36ebb0a974
psycholasma <psychoplasma@gmail.com>
2017-12-01 00:58:50 +0900

    Add splash&login screen UIs

--- app/src/main/java/com/nowwego/hakwonga/activities/SplashActivity.java
+++ app/src/main/java/com/nowwego/hakwonga/activities/SplashActivity.java
@@ -13,6 +13,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        // TODO: Later remove sleeping because there will be already some time consuming background processes here
+        // Wait a little bit on the splash screen
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // There is nothing to do here.
+        }
+
         Intent i = LoginActivity.getStartIntent(getApplicationContext());
         startActivity(i);
         finish();
